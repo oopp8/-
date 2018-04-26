d7c0c829e35a4ce854229097ae97412b34571048
oopp8 <oopp8hexadecimal@sina.com>
2018-04-26 10:10:57 +0800

    Add files via upload

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开设备";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(46, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "关闭设备";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(182, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 51);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "录入指纹";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(182, 104);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 51);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "识别指纹";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(182, 218);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 51);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "指纹库";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(307, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 280);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(182, 161);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 51);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "清空指纹";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(46, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "获取指纹图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(466, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 240);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 318);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ test/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        SynoAPIExHelper fh = new SynoAPIExHelper();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var v = fh.OpenDevice();
+            label1.Text = v.ToString();
+            if (v == ReturnValue.PS_OK)
+            {
+                foreach (var item in this.Controls)
+                {
+                    if (item.GetType() == typeof(Button))
+                    {
+                        (item as Button).Enabled = true;
+                    }
+                }
+                button1.Enabled = false;
+                button6.PerformClick();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = fh.CloseDevice().ToString();
+            foreach (var item in this.Controls)
+            {
+                if (item.GetType() == typeof(Button))
+                {
+                    (item as Button).Enabled = false;
+                }
+            }
+            button1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = "finger.bmp";
+            if (pictureBox1.BackgroundImage != null)
+                pictureBox1.BackgroundImage.Dispose();
+            pictureBox1.BackgroundImage = null;
+            File.Delete(path);
+            label1.Text = fh.SaveFigerBmp(path).ToString();
+            if (File.Exists(path))
+            {
+                Bitmap b = new Bitmap(path);
+                pictureBox1.BackgroundImage = b;
+                pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            else
+            {
+                pictureBox1.BackgroundImage = null;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            label1.Text = fh.AddFinger(out n).ToString();
+            label2.Text = n.ToString();
+            button6.PerformClick();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            label1.Text = fh.FindFinger(out n).ToString();
+            label2.Text = n.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            List<int> ll = new List<int>();
+            label1.Text = fh.GetAllFinger(out ll).ToString();
+            foreach (var item in ll)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = fh.ClearAllFinger().ToString();
+            button6.PerformClick();
+        }
+    }
+}
